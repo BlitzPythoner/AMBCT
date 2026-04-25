c4704f5746e3803d97fe34c92470456f6ffe52fd
Alex Blitz <alexblitz886@outlook.de>
2026-04-25 14:20:56 +0200

    Delete Source directory

--- Source/bench/read.h
+++
@@ -1,6 +0,0 @@
-#ifndef READ_H
-#define READ_H
-
-double read_test(const char* file);
-
-#endif
--- Source/bench/write.h
+++
@@ -1,6 +0,0 @@
-#ifndef WRITE_H
-#define WRITE_H
-
-double write_test(const char* path, char* out_file);
-
-#endif
